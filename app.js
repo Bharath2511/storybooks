0ba3b3c7396bda258c9f4feb3c333ac47578d960
V S Bharath <bharathcse5t6@gmail.com>
2018-10-10 19:56:03 +0530

    making redirect page work

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const passport = require('passport')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
 //load user model
 require('./models/User')
 
@@ -24,9 +26,27 @@ app.get('/',(req,res)=>{
 res.send('it works')
 })
 
+app.use(cookieParser())
+app.use(session({
+    secret:'secret',
+    resave : false,
+    saveUninitialized:false
+}))
+
+
+
+// passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+ //set global vars
+ app.use((req,res,next)=>{
+     res.locals.user = req.user || null
+     next()
+ })
+
 //Use Routes
 app.use('/auth',auth)
- 
+
 const port = process.env.port || 5000
 
 app.listen(port,()=>{
--- config/passport.js
+++ config/passport.js
@@ -41,4 +41,10 @@ module.exports = function(passport) {
             }))
         })
     )
+    passport.serializeUser((user,done)=>{
+        done(null,user.id)
+    })
+    passport.deserializeUser((id,done)=>{
+        User.findById(id).then(user => done(null,user))
+    })
 }

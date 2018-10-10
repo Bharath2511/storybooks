9eea72c88d074adde9608ceb8046c51aa7356634
V S Bharath <bharathcse5t6@gmail.com>
2018-10-10 18:06:37 +0530

    understanding google oauth

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const passport = require('passport')
+
+//passport config 
+require('./config/passport')(passport)
+//load routes 
+const auth = require('./routes/auth')
 
 const app = express()
 
@@ -7,6 +13,9 @@ app.get('/',(req,res)=>{
 res.send('it works')
 })
 
+//Use Routes
+app.use('/auth',auth)
+ 
 const port = process.env.port || 5000
 
 app.listen(port,()=>{

f409dc690085d29d3e8e7ca9808a9fe6dca4cf31
V S Bharath <bharathcse5t6@gmail.com>
2018-10-10 22:09:56 +0530

    designing our ui

--- app.js
+++ app.js
@@ -1,54 +1,64 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const passport = require('passport')
-const cookieParser = require('cookie-parser')
-const session = require('express-session')
-//load user model
-require('./models/User')
+const express = require('express');
+const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const passport = require('passport');
 
-//passport config 
-require('./config/passport')(passport)
-//load routes 
-const auth = require('./routes/auth')
-//load keys
-const keys = require('./config/keys')
-//map global promises
-mongoose.Promise = global.Promise
-//mongoose connect
-mongoose.connect(keys.mongoURI,{
-    useMongoClient:true
-}).then(()=>console.log('mongodb connected'))
-.catch(err => console.log(err))
+// Load User Model
+require('./models/User');
 
-const app = express()
+// Passport Config
+require('./config/passport')(passport);
 
-app.get('/',(req,res)=>{
-res.send('it works')
+// Load Routes
+const index = require('./routes/index');
+const auth = require('./routes/auth');
+
+// Load Keys
+const keys = require('./config/keys');
+
+// Map global promises
+mongoose.Promise = global.Promise;
+// Mongoose Connect
+mongoose.connect(keys.mongoURI, {
+  useMongoClient:true
 })
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
-app.use(cookieParser())
+const app = express();
+
+// Handlebars Middleware
+app.engine('handlebars', exphbs({
+  defaultLayout:'main'
+}));
+app.set('view engine', 'handlebars');
+
+app.use(cookieParser());
 app.use(session({
-    secret:'secret',
-    resave : false,
-    saveUninitialized:false
-}))
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false
+}));
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Set global vars
+app.use((req, res, next) => {
+  res.locals.user = req.user || null;
+  next();
+});
+
+// Use Routes
+app.use('/', index);
+app.use('/auth', auth);
 
 
+const port = process.env.PORT || 5000;
 
-// passport middleware
-app.use(passport.initialize())
-app.use(passport.session())
- //set global vars
- app.use((req,res,next)=>{
-     res.locals.user = req.user || null
-     next()
- })
-
-//Use Routes
-app.use('/auth',auth)
-
-const port = process.env.port || 5000
-
-app.listen(port,()=>{
-    console.log(`Server started on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`)
+});
